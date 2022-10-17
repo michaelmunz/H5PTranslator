ce98db9812f42b54c7da1e2ab2edc30f5c8751b0
Michael Munz <michael.munz@thu.de>
2022-10-17 12:05:32 +0200

    change main tests

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 from h5ptranslate.h5ptranslate import H5PTranslator, H5PTranslatorImpl
 
 h5ptrans = H5PTranslatorImpl()
-ori_file = os.path.abspath(r"./data/course-presentation-36_DE.h5p")
-translate_file = os.path.abspath(r"./data/course-presentation-36_EN.h5p")
+ori_file = os.path.abspath(r"./data/course-presentation-58.h5p")
+translate_file = os.path.abspath(r"./data/course-presentation-58_EN.h5p")
 
 h5ptrans.open(ori_file, translate_file)
 
@@ -13,14 +13,16 @@ print("Translate temp dir: "+h5ptrans.getTemporaryDir_translate())
 
 print("Nr of slides: {}".format(h5ptrans.getNrOfSlides()))
 slideNr = 1
-print("Nr of Elements in slide nr. {}: {}".format(slideNr, h5ptrans.getNrOfElementsForSlide(slideNr)))
+print("Nr of Elements in slide nr. {}: {}".format(slideNr, len(h5ptrans.getElementsForSlide_original(slideNr))))
 #print("Text of first element: {}".format(h5ptrans.getElementsForSlide_original(slideNr)[0].getText()))
 
 
 modified_ids = h5ptrans.getModifiedElementIDs()
 untranslated_ids = h5ptrans.getUntranslatedElementIDs()
 
-print("Modified ids: "+str(modified_ids))
-print("Untranslated ids: "+str(untranslated_ids))
+print("Modified ids (%d): %s" %(len(modified_ids), str(modified_ids)))
+print("Untranslated ids (%d): %s" %(len(untranslated_ids), str(untranslated_ids)))
 h5ptrans.setTranslation(untranslated_ids[0], "This is an english test for the first id!")
 h5ptrans.close(True)
+
+
