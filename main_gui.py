c8f2b68e630c64ff4cf1c5f22e04d94203f3001a
Michael Munz <michael.munz@thu.de>
2022-11-14 16:24:32 +0100

    finished gui

--- main_gui.py
+++ main_gui.py
@@ -1,11 +1,12 @@
 import tkinter as tk
+from threading import Thread
 from tkinter import ttk
 from tkinter import filedialog as fd, messagebox
 import os
 from h5p_python.h5ptranslate import H5PTranslatorImpl
 
 
-class main_gui(tk.Tk):
+class MainGUI(tk.Tk):
 
     def __init__(self):
         super().__init__()
@@ -36,25 +37,28 @@ class main_gui(tk.Tk):
                                        command=self.do_translate, state=tk.DISABLED
                                        )
         self.btn_translate.grid(row=2,column=0, columnspan=2)
-
-
+        self.progressbar = ttk.Progressbar(self, orient='horizontal', mode='determinate', length=100)
+        self.progressbar.grid(row=3, column=0, columnspan=2)
 
         self.btn_replace_images = tk.Button(text="Replace images",
                                             width=25,
                                             height=5,
                                             command=self.do_replace_images, state=tk.DISABLED
                                             )
-        self.btn_replace_images.grid(row=3,column=0, columnspan=2)
+        self.btn_replace_images.grid(row=4,column=0, columnspan=2)
 
-        ttk.Label(text="Untranslated IDs:").grid(row=4,column=0, columnspan=2)
+        ttk.Label(text="Untranslated IDs:").grid(row=5,column=0, columnspan=2)
         self.txt_untranslated_ids = tk.Text(height=10, width=40)
-        self.txt_untranslated_ids.grid(row=5,column=0, columnspan=2)
+        self.txt_untranslated_ids.grid(row=6,column=0, columnspan=2)
 
-        ttk.Label(text="Modified IDs:").grid(row=6,column=0, columnspan=2)
+        ttk.Label(text="Modified IDs:").grid(row=7,column=0, columnspan=2)
         self.txt_modified_ids = tk.Text(height=10, width=40)
-        self.txt_modified_ids.grid(row=7,column=0, columnspan=2)
+        self.txt_modified_ids.grid(row=8,column=0, columnspan=2)
+
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.translate_thread = None
+
 
 
     def on_target_lang_selected(self, val):
@@ -85,26 +89,45 @@ class main_gui(tk.Tk):
         self.btn_translate.config(state=tk.NORMAL)
         self.h5ptrans.open(self.ori_file, self.translate_file)
 
+
+        self.update_h5pdata()
+
+
+    def update_h5pdata(self):
         untranslated_ids = self.h5ptrans.getUntranslatedElementIDs()
         modified_ids = self.h5ptrans.getModifiedElementIDs()
+        self.txt_untranslated_ids.delete("1.0", "end")
         for t in untranslated_ids:
-            self.txt_untranslated_ids.insert(tk.END, t+"\n")
+            self.txt_untranslated_ids.insert(tk.END, t + "\n")
 
+        self.txt_modified_ids.delete("1.0", "end")
         for t in modified_ids:
-            self.modified_ids.insert(tk.END, t+"\n")
-
-        self.txt_modified_ids = modified_ids
+            self.txt_modified_ids.insert(tk.END, t + "\n")
 
 
-    def do_translate(self):
-        untranslated_ids = self.h5ptrans.getUntranslatedElementIDs()
-        for id in untranslated_ids:
+
+    def translate_worker(self, untranslated_ids):
+        for cnt,id in enumerate(untranslated_ids):
             elem = self.h5ptrans.getElementByID_original(id)
             print("Autotranslating id '{}': '{}'".format(id, elem.getText()))
-            autotranslated_text = self.h5ptrans.getAutoTranslation("en", "de", elem.getText())
+            autotranslated_text = self.h5ptrans.getAutoTranslation("en", self.target_lang, elem.getText())
             print("Result: " + autotranslated_text)
             self.h5ptrans.setTranslation(id, autotranslated_text)
+            self.progressbar.config(value=cnt/len(untranslated_ids)*100)
+
+        self.progressbar.stop()
         self.btn_replace_images.config(state=tk.NORMAL)
+        self.update_h5pdata()
+
+    def do_translate(self):
+        ids_to_translate = self.h5ptrans.getUntranslatedElementIDs() + self.h5ptrans.getModifiedElementIDs()
+        if len(ids_to_translate)==0:
+            return
+
+        self.translate_thread = Thread(target=self.translate_worker, args=(ids_to_translate,))
+        self.translate_thread.start()
+
+
 
     def do_replace_images(self):
         dirname = fd.askdirectory(title="Select directory containing tranlated images")
@@ -131,6 +154,6 @@ class main_gui(tk.Tk):
             pass
 
 if __name__ == "__main__":
-    gui = main_gui()
+    gui = MainGUI()
     gui.mainloop()
 
