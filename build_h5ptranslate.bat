6b990724e9d372be292294e368ac0d8886ee9dca
Michael Munz <michael.munz@thu.de>
2022-12-05 14:17:00 +0100

    add version to build

--- build_h5ptranslate.bat
+++ build_h5ptranslate.bat
@@ -1,6 +1,7 @@
 call C:\ProgramData\Anaconda3\condabin\activate.bat C:\Users\micha\THU\H5PTranslator\env
 cd C:\Users\micha\THU\H5PTranslator
-git tag -l --contains HEAD >version.txt
+
+git tag -l --contains HEAD > version.txt
 git rev-parse --short HEAD >> version.txt
-REM pyinstaller main_gui.py
+REM pyinstaller main_gui.py  --add-data "/my/path/to/mytextfile.txt:/path/mytextfile.txt"
 pause
